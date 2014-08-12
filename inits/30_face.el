bb31c4689393c7385072d93322cb156e31a4a573
murase_syuka <murase.syuka+github@gmail.com>
2014-08-12 23:23:33 +0900

    add emacs color theme

---
+++ inits/30_face.el
@@ -0,0 +1,14 @@
+;; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
+
+(require 'color-theme)
+(color-theme-initialize)
+
+;(color-theme-katester)
+(color-theme-euphoria)
+
+
+
+(require 'rainbow-delimiters)
+;(add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
+(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
+
