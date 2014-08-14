3037fb360bb7d5348f7b5ac03e5f04204841fe23
murase_syuka <murase.syuka+github@gmail.com>
2014-08-14 18:34:11 +0900

    refactoring yascroll.el

--- inits/30_face.el
+++ inits/30_face.el
@@ -15,3 +15,11 @@
 ;(add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
 (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
 ;(global-rainbow-delimiters-mode)
+
+
+
+;;; scrollバー
+;; http://d.hatena.ne.jp/m2ym/20110401/1301617991
+(require 'yascroll)
+(global-yascroll-bar-mode 1)
+
