72fa016939d2716b1ac99bccb868472c936f26eb
murase_syuka <murase.syuka+github@gmail.com>
2014-08-14 19:12:55 +0900

    rainbow-delimiters using stronger colors

--- inits/30_face.el
+++ inits/30_face.el
@@ -1,4 +1,4 @@
-;; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
+;;; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
 
 (load-theme 'misterioso t)
 
@@ -15,6 +15,16 @@
 ;(add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
 (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
 ;(global-rainbow-delimiters-mode)
+
+;; http://yoo2080.wordpress.com/2013/12/21/small-rainbow-delimiters-tutorial/
+; ;; 5.1. using stronger colors
+(require 'cl-lib)
+(require 'color)
+(cl-loop
+ for index from 1 to rainbow-delimiters-max-face-count
+ do
+ (let ((face (intern (format "rainbow-delimiters-depth-%d-face" index))))
+   (cl-callf color-saturate-name (face-foreground face) 30)))
 
 
 
