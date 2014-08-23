ea550e79256d6a4a1483a0c3b3e182fdac743021
murase_syuka <murase.syuka+github@gmail.com>
2014-08-23 11:24:24 +0900

    add lint check javascript

--- inits/70_flycheck.el
+++ inits/70_flycheck.el
@@ -1,3 +1,9 @@
 ;; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
 
 (add-hook 'after-init-hook #'global-flycheck-mode)
+
+
+;;; ??-javascript.el
+;; http://qiita.com/senda-akiha/items/cddb02cfdbc0c8c7bc2b
+(flycheck-add-next-checker 'javascript-jshint
+                           'javascript-gjslint)
