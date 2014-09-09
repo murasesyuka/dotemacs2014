c36ea65f2cb4914e68e75968b51de649acd54047
murase_syuka <murase.syuka+github@gmail.com>
2014-09-09 20:32:29 +0900

    adhoc (require 'elisp-slime-nav)

--- inits/50_elisp.el
+++ inits/50_elisp.el
@@ -1,4 +1,5 @@
 ;; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
 
+(require 'elisp-slime-nav) ;; optional if installed via package.el
 (dolist (hook '(emacs-lisp-mode-hook ielm-mode-hook lisp-interaction-mode-hook))
   (add-hook hook 'turn-on-elisp-slime-nav-mode))
