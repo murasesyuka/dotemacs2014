85109d3f854476bf4188d99607ddcb8c752a3a7f
murase_syuka <murase.syuka+github@gmail.com>
2014-09-09 21:07:46 +0900

    enable e2wm-vcs

--- inits/30_e2wm.el
+++ inits/30_e2wm.el
@@ -2,23 +2,17 @@
 
 ;;; e2wm
 ; http://d.hatena.ne.jp/kiwanami/20100528/1275038929
-;;; 
-;; (auto-install-from-url "http://github.com/kiwanami/emacs-window-layout/raw/master/window-layout.el")
-;; (auto-install-from-url "http://github.com/kiwanami/emacs-window-manager/raw/master/e2wm.el")
-
-
 (require 'e2wm)
 (global-set-key (kbd "M-+") 'e2wm:start-management)
 (global-set-key (kbd "C-9") 'e2wm:dp-code-main-maximize-toggle-command)
 
 
-;; (auto-install-from-url "https://github.com/kiwanami/emacs-window-manager/raw/master/e2wm-vcs.el")
 
-;; (require 'magit)
-;; (require 'e2wm-vcs)
+(require 'magit)
+(require 'e2wm-vcs)
 
-;; ;; M-w に magit パースペクティブ変更を割り当て
-;; (e2wm:add-keymap
-;;  e2wm:pst-minor-mode-keymap
-;;  '(("C-M-w" . e2wm:dp-magit))
-;;  e2wm:prefix-key)
+;; M-w に magit パースペクティブ変更を割り当て
+(e2wm:add-keymap
+ e2wm:pst-minor-mode-keymap
+ '(("C-M-w" . e2wm:dp-magit))
+ e2wm:prefix-key)
