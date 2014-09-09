b8c998a0ed67f3ddbdf004a7772406495a686a63
murase_syuka <murase.syuka+github@gmail.com>
2014-09-09 20:31:02 +0900

    disable jslint etc

--- inits/70_flycheck.el
+++ inits/70_flycheck.el
@@ -3,7 +3,7 @@
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
 
-;;; ??-javascript.el
-;; http://qiita.com/senda-akiha/items/cddb02cfdbc0c8c7bc2b
-(flycheck-add-next-checker 'javascript-jshint
-                           'javascript-gjslint)
+;; ;;; ??-javascript.el
+;; ;; http://qiita.com/senda-akiha/items/cddb02cfdbc0c8c7bc2b
+;; (flycheck-add-next-checker 'javascript-jshint
+;;                            'javascript-gjslint)
