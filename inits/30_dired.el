0cf17e63149c7fb45d3355de51fc29e75ae4546a
murase_syuka <murase.syuka+github@gmail.com>
2014-09-09 20:34:40 +0900

    fix direx config

--- inits/30_dired.el
+++ inits/30_dired.el
@@ -7,7 +7,8 @@
 
 (push '(direx:direx-mode :position left :width 25 :dedicated t)
       popwin:special-display-config)
-(global-set-key (kbd "C-x C-j") 'direx:jump-to-directory-other-window)
+;(global-set-key (kbd "C-x C-j") 'direx:jump-to-directory-other-window)
+(global-set-key (kbd "C-c C-f") 'direx:jump-to-directory-other-window)
 
 ;;; http://d.hatena.ne.jp/khiker/20070129/nyumon_gnu_emacs
 ;;; 少し使いやすいファイラ
