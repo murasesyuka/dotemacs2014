1827ea87f146dd1dcf0e9329f0070434fceb5580
murase_syuka <murase.syuka+github@gmail.com>
2014-09-17 01:02:28 +0900

    trivial fix

--- inits/50_java.el
+++ inits/50_java.el
@@ -1,10 +1,14 @@
 ;; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
 
 ;;http://d.hatena.ne.jp/mzp/20090618/emacs
-(add-hook 'java-mode-hook
-	  (lambda ()
-	    (message "hook")
-	    (setq tab-width 4)
-	    (setq indent-tabs-mode t)
-	    (setq c-basic-offset 4)))
+(defun my-java-offset()
+  ;; (message "hook")
+  (setq tab-width 4)
+  (setq indent-tabs-mode t)
+  (setq c-basic-offset 4)
+  )
+
+(add-hook 'java-mode-hook 'my-java-offset)
+
+
 
