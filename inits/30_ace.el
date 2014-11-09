665571f225cb3c231af009d1169e233ca760a05f
murase_syuka <murase.syuka+github@gmail.com>
2014-11-10 08:55:39 +0900

    enable ace-isearch, for changed default isearch

--- inits/30_ace.el
+++ inits/30_ace.el
@@ -1,5 +1,7 @@
 ;; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
 
+(global-ace-isearch-mode 1)
+
 ;; ヒント文字に使う文字を指定する
 (setq ace-jump-mode-move-keys
       (append "asdfghjkl;:]qwertyuiop@zxcvbnm,." nil))
