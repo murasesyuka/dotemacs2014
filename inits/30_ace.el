a417823fc152cdd07b30641405a6172ee46e7e24
murase_syuka <murase.syuka+github@gmail.com>
2014-11-10 08:19:47 +0900

    cp ace-jump default config

---
+++ inits/30_ace.el
@@ -0,0 +1,10 @@
+;; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
+
+;; ヒント文字に使う文字を指定する
+(setq ace-jump-mode-move-keys
+      (append "asdfghjkl;:]qwertyuiop@zxcvbnm,." nil))
+;; ace-jump-word-modeのとき文字を尋ねないようにする
+(setq ace-jump-word-mode-use-query-char nil)
+(global-set-key (kbd "C-:") 'ace-jump-char-mode)
+(global-set-key (kbd "C-;") 'ace-jump-word-mode)
+(global-set-key (kbd "C-M-;") 'ace-jump-line-mode)
