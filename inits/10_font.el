d7ab50e4df5965d98a1b7a7507d209dfdee394b4
murase_syuka <murase.syuka+github@gmail.com>
2015-03-04 14:34:21 +0900

    change font size

--- inits/10_font.el
+++ inits/10_font.el
@@ -3,4 +3,6 @@
 ;;; フォント設定
 
 ;; https://github.com/yascentur/Ricty/tree/3.2.1
-(add-to-list 'default-frame-alist '(font . "ricty-12"))
+;(add-to-list 'default-frame-alist '(font . "ricty-12"))	;11inch HD
+(add-to-list 'default-frame-alist '(font . "ricty-18")) ;15inch 4k
+
