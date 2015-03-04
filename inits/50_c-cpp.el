34542ddebe81454c68d8fb2b14c67ba06d0a27a0
murase_syuka <murase.syuka+github@gmail.com>
2015-03-04 14:43:28 +0900

    fix c/c++/java code offset

--- inits/50_c-cpp.el
+++ inits/50_c-cpp.el
@@ -13,35 +13,35 @@
 
 ;; (setq-default c-basic-offset 4     ;;基本インデント量4
 ;;               tab-width 4          ;;タブ幅4
-;;               indent-tabs-mode nil)  ;;インデントをタブでするかスペースでするか
+;;               indent-tabs-mode -1)  ;;インデントをタブでするかスペースでするか
 
 
-(defun my-c-eldoc()
+(defun my/c-eldoc()
   (set (make-local-variable 'eldoc-idle-delay) 0.20)
   (c-turn-on-eldoc-mode)
   )
 
-(defun my-c-offset()
+(defun my/c-offset()
   (c-set-style "linux")
-  ;; (setq c-basic-offset 8)
-  ;; (setq tab-width 8)
-  ;; (setq indent-tabs-mode t)	; インデントは空白文字で行う（TABコードを空白に変換）
-  ;; (setq c-auto-newline t) ; オートインデント オン
+  (setq c-basic-offset 8)
+  (setq tab-width 8)
+  (setq indent-tabs-mode -1)	; インデントは空白文字で行う（TABコードを空白に変換）
+  ;xb; (setq c-auto-newline t) ; オートインデント オン
   )
 
-(defun my-c++-offset()
+(defun my/c++-offset()
   (c-set-style "stroustrup")
-  ;; (setq indent-tabs-mode nil)     ; インデントは空白文字で行う（TABコードを空白に変換）
-  ;; (setq c-basic-offset 4)
-  ;; (setq tab-width 4)
+  (setq indent-tabs-mode 1)     ; インデントは空白文字で行う（TABコードを空白に変換）
+  (setq c-basic-offset 4)
+  (setq tab-width 4)
   ;; (c-set-offset 'innamespace 0)   ; namespace {}の中はインデントしない
   ;; (c-set-offset 'arglist-close 0) ; 関数の引数リストの閉じ括弧はインデントしない
   )
 
-(add-hook 'c-mode-hook   'my-c-offset)
-(add-hook 'c-mode-hook   'my-c-eldoc)
-(add-hook 'c++-mode-hook 'my-c++-offset)
+(add-hook 'c-mode-hook   'my/c-offset)
+(add-hook 'c-mode-hook   'my/c-eldoc)
+(add-hook 'c++-mode-hook 'my/c++-offset)
 
-;(remove-hook 'c-mode-hook 'my-c-offset)
-;(remove-hook 'c-mode-hook   'my-c-eldoc)
-;(remove-hook 'c++-mode-hook 'my-c++-offset)
+;(remove-hook 'c-mode-hook 'my/c-offset)
+;(remove-hook 'c-mode-hook   'my/c-eldoc)
+;(remove-hook 'c++-mode-hook 'my/c++-offset)
--- inits/50_java.el
+++ inits/50_java.el
@@ -1,14 +1,14 @@
 ;; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
 
 ;;http://d.hatena.ne.jp/mzp/20090618/emacs
-(defun my-java-offset()
+(defun my/java-offset()
   ;; (message "hook")
   (setq tab-width 4)
   (setq indent-tabs-mode t)
   (setq c-basic-offset 4)
   )
 
-(add-hook 'java-mode-hook 'my-java-offset)
+(add-hook 'java-mode-hook 'my/java-offset)
 
 
 
