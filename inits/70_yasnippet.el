f34a7d69a9dd455fc4b8529781b238a21ac4f8fd
murase_syuka <murase.syuka+github@gmail.com>
2016-01-07 21:40:34 +0900

    trivialfix

--- inits/70_yasnippet.el
+++ inits/70_yasnippet.el
@@ -1,5 +1,7 @@
 ;; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
 
+;;; http://keisanbutsuriya.hateblo.jp/entry/2015/12/12/181028
+
 (require 'yasnippet)
 
 (setq yas-snippet-dirs
@@ -13,6 +15,7 @@
   '(progn
      (setq helm-c-yas-space-match-any-greedy t) ;[default: nil]
      
+     (global-set-key (kbd "C-c y") 'helm-c-yas-complete)
      (global-set-key (kbd "C-.") 'helm-c-yas-complete)
 
      ;; (add-to-list 'yas-extra-mode-hooks 'ruby-mode-hook)
